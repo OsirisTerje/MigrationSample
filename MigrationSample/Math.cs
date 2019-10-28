553c4817c0af0db62e7b4725568c6b41ea86e9de
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2019-10-28 14:35:39 +0100

    Add project files.

---
+++ MigrationSample/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MigrationSample
+{
+    public class Math
+    {
+        public int LastResult { get; private set; }
+        public int Add(int a, int b)
+        {
+            LastResult =  a + b;
+            return LastResult;
+        }
+
+        public string AsJson()
+        {
+            string output = JsonConvert.SerializeObject(this);
+            return output;
+        }
+    }
+}
---
+++ MigrationSampleTests/MathTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using MigrationSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationSample.Tests
+{
+    [TestFixture()]
+    public class MathTests
+    {
+        [Test()]
+        public void AddTest()
+        {
+            var sut = new Math();
+            var result = sut.Add(2, 3);
+            var json = sut.AsJson();
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(5));
+                Assert.That(json, Is.EqualTo("{\"LastResult\":5}"));
+            });
+        }
+    }
+}
